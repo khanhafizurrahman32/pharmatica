254a26fa1c57ce7914140a7023748b5e2067ff52
khanhafizurrahman32 <khanhafizurrahman32@gmail.com>
2024-07-28 22:37:37 +0600

    Initial project with relevant dependency and structure

---
+++ src/main/java/org/example/pharmaticb/PharmaticBApplication.java
@@ -0,0 +1,13 @@
+package org.example.pharmaticb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaticBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaticBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/pharmaticb/PharmaticBApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pharmaticb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaticBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
