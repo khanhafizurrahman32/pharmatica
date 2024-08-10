c34644cf20e3a94c907366f441b36d3a44e5ad6e
khanhafizurrahman32 <khanhafizurrahman32@gmail.com>
2024-08-10 17:26:12 +0600

    Apply new .gitignore rules

---
+++ src/main/java/org/example/pharmaticb/service/minio/MinioService.java
@@ -0,0 +1,45 @@
+package org.example.pharmaticb.service.minio;
+
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class MinioService {
+    private final MinioClient minioClient;
+
+    public void uploadFile(String minioBucket, String key, byte[] fileBuffer, String contentType) {
+        try {
+            boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket(minioBucket).build());
+            if (!found) {
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(minioBucket).build());
+            }
+
+            InputStream inputStream = new ByteArrayInputStream(fileBuffer);
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(minioBucket)
+                            .object(key)
+                            .stream(inputStream, fileBuffer.length, -1)
+                            .contentType(contentType)
+                            .build()
+            );
+            inputStream.close();
+        } catch (Exception ex) {
+            log.error("File upload error" + ex.getMessage());
+        }
+
+
+    }
+}
