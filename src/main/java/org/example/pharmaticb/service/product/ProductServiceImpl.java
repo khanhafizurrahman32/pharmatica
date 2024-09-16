86b86ee17b8237301f8596b0ddf3e098f75eab9b
khanhafizurrahman32 <khanhafizurrahman32@gmail.com>
2024-09-16 18:03:07 +0600

    git commit -m "Handle different edge cases for bulk product creation"

--- src/main/java/org/example/pharmaticb/service/product/ProductServiceImpl.java
+++ src/main/java/org/example/pharmaticb/service/product/ProductServiceImpl.java
@@ -10,6 +10,7 @@ import org.example.pharmaticb.Models.Request.BulkProductCreateRequest;
 import org.example.pharmaticb.Models.Request.ProductRequest;
 import org.example.pharmaticb.Models.Response.*;
 import org.example.pharmaticb.dto.ProductWithDetails;
+import org.example.pharmaticb.exception.InternalException;
 import org.example.pharmaticb.repositories.BrandRepository;
 import org.example.pharmaticb.repositories.CategoryRepository;
 import org.example.pharmaticb.repositories.CountryRepository;
@@ -18,6 +19,8 @@ import org.example.pharmaticb.service.brand.BrandService;
 import org.example.pharmaticb.service.category.CategoryService;
 import org.example.pharmaticb.service.country.CountryService;
 import org.example.pharmaticb.service.file.FileUploadService;
+import org.example.pharmaticb.utilities.DateUtil;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 import reactor.core.publisher.Flux;
@@ -26,7 +29,15 @@ import reactor.core.publisher.Mono;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.example.pharmaticb.utilities.DateUtil.BULK_PRODUCT_INPUT_FORMATTER;
 
 @Slf4j
 @Service
@@ -110,10 +121,20 @@ public class ProductServiceImpl implements ProductService {
                                 .skip(1)
                 ))
                 .map(this::parseProductFromCsvLine)
-                .flatMap(productMono -> productMono)
-                .flatMap(this::insertProductIntoDatabase)
+                .flatMap(productMono -> productMono
+                        .doOnNext(product -> log.debug("Product emitted: {}", product))
+                        .doOnError(error -> log.error("Error in productMono: ", error))
+                        .switchIfEmpty(Mono.fromRunnable(() -> log.warn("Empty productMono")))
+                )
+                .log("After flatMap")  // This will log all signals at this point in the stream
+                .doOnNext(product -> log.debug("Parsed product: {}", product))
+                .flatMap(product -> {
+                    log.info("product:: {}", product.getId());
+                    return insertProductIntoDatabase(product);
+                })
                 .collectList()
-                .map(ret ->BulkProductCreateResponse.builder().success(true).build());
+                .map(ret -> BulkProductCreateResponse.builder().success(true).build())
+                .log("Final result");
     }
 
     private Mono<Product> insertProductIntoDatabase(Product product) {
@@ -121,38 +142,100 @@ public class ProductServiceImpl implements ProductService {
     }
 
     private Mono<Product> parseProductFromCsvLine(String line) {
-        String[] parts = line.split(",");
-        return Mono.zip(getCategoryId(parts[3]), getBrandId(parts[5]), getCountryId(parts[7]))
-                .map(tuple3 -> Product.builder()
-                        .productName(parts[0])
-                        .price(Double.parseDouble(parts[1]))
-                        .imageUrl(parts[2])
-                        .categoryId(tuple3.getT1())
-                        .discount(Double.parseDouble(parts[4]))
-                        .brandId(tuple3.getT2())
-                        .expires(parts[6])
-                        .countryId(tuple3.getT3())
-                        .description(parts[8])
-                        .howToUse(parts[9])
-                        .ingredients(parts[10])
-                        .stock(Double.parseDouble(parts[11]))
-                        .composition(parts[12])
-                        .build());
+        List<String> parts = parseLine(line);
+
+        if (parts.size() < 13) {
+            return Mono.error(new IllegalArgumentException("CSV line does not have enough fields: " + line));
+        }
+
+        return Mono.zip(
+                        getCategoryId(parts.get(3)).doOnNext(id -> log.info("Category ID: {}", id)),
+                        getBrandId(parts.get(5)).doOnNext(id -> log.info("Brand ID: {}", id)),
+                        getCountryId(parts.get(7)).doOnNext(id -> log.info("Country ID: {}", id))
+                )
+                .flatMap(tuple3 -> {
+                    try {
+                        Product product = Product.builder()
+                                .productName(parts.get(0))
+                                .price(Double.parseDouble(parts.get(1)))
+                                .imageUrl(parts.get(2))
+                                .categoryId(tuple3.getT1())
+                                .discount(getDiscount(parts.get(1), parts.get(4)))
+                                .brandId(tuple3.getT2())
+                                .expires(getExpireDate(parts.get(6)))
+                                .countryId(tuple3.getT3())
+                                .description(parts.get(8))
+                                .howToUse(parts.get(9))
+                                .ingredients(parts.get(10))
+                                .stock(Double.parseDouble(parts.get(11)))
+                                .composition(parts.get(12))
+                                .build();
+                        return Mono.just(product);
+                    } catch (NumberFormatException e) {
+                        return Mono.error(new IllegalArgumentException("Failed to parse numeric value in line: " + line, e));
+                    }
+                })
+                .doOnNext(product -> log.debug("Successfully parsed product: {}", product))
+                .doOnError(error -> log.error("Error parsing product from line: {}. Error: {}", line, error.getMessage()));
+    }
+
+    public LocalDate parseDate(String dateString) {
+        try {
+            return LocalDate.parse(dateString, BULK_PRODUCT_INPUT_FORMATTER);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("Could not parse date: " + dateString, e);
+        }
+    }
+    private String getExpireDate(String expiresDate) {
+        try {
+            LocalDate date = parseDate(expiresDate);
+            return DateUtil.productExpiresFormat(date);
+        } catch (DateTimeParseException e) {
+            log.error("could not parse date {}", expiresDate, e);
+            throw new InternalException(HttpStatus.BAD_REQUEST, "could not parse date " + expiresDate, "EXPIRE_DATE_01");
+        }
+    }
+
+    private double getDiscount(String price, String discount) {
+        try {
+            BigDecimal priceValue = new BigDecimal(price.trim());
+            BigDecimal discountPercentage = new BigDecimal(discount.trim());
+
+            if (priceValue.compareTo(BigDecimal.ZERO) < 0) {
+                throw new IllegalArgumentException("Price cannot be negative");
+            }
+
+            if (discountPercentage.compareTo(BigDecimal.ZERO) < 0 || discountPercentage.compareTo(BigDecimal.valueOf(100)) > 0) {
+                throw new IllegalArgumentException("Discount percentage must be between 0 and 100");
+            }
+
+            BigDecimal discountFactor = discountPercentage.divide(BigDecimal.valueOf(100), 4, RoundingMode.HALF_UP);
+            BigDecimal result = priceValue.multiply(discountFactor).setScale(2, RoundingMode.HALF_UP);
+            return result.doubleValue();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid number format: " + e.getMessage(), e);
+        }
     }
 
     private Mono<Long> getCategoryId(String label) {
         return categoryRepository.findByLabel(label)
-                .map(Category::getId);
+                .map(Category::getId)
+                .switchIfEmpty(Mono.defer(() -> categoryRepository.save(Category.builder().label(label).build()))
+                        .map(Category::getId));
     }
 
     private Mono<Long> getBrandId(String label) {
         return brandRepository.findByBrandName(label)
-                .map(Brand::getId);
+                .map(Brand::getId)
+                .switchIfEmpty(Mono.defer(() -> brandRepository.save(Brand.builder().brandName(label).build()))
+                        .map(Brand::getId));
     }
 
     private Mono<Long> getCountryId(String label) {
         return countryRepository.findByCountryName(label)
-                .map(Country::getId);
+                .map(Country::getId)
+                .switchIfEmpty(Mono.defer(() -> countryRepository.save(Country.builder().countryName(label).build()))
+                        .map(Country::getId));
     }
 
     private Mono<CountryResponse> getCountryResponse(long countryId) {
@@ -240,5 +323,32 @@ public class ProductServiceImpl implements ProductService {
                 .stock(Double.parseDouble(request.getStock()))
                 .coupons(request.getCoupons())
                 .build();
+    }
+
+    public static List<String> parseLine(String line) {
+        List<String> result = new ArrayList<>();
+        StringBuilder currentField = new StringBuilder();
+        boolean inQuotes = false;
+
+        for (int i = 0; i < line.length(); i++) {
+            char c = line.charAt(i);
+
+            if (c == '"') {
+                // Toggle the inQuotes flag when we encounter a quote
+                inQuotes = !inQuotes;
+            } else if (c == ',' && !inQuotes) {
+                // If we're not in quotes and we find a comma, we've reached the end of a field
+                result.add(currentField.toString().trim());
+                currentField = new StringBuilder();
+            } else {
+                // Otherwise, add the character to the current field
+                currentField.append(c);
+            }
+        }
+
+        // Add the last field
+        result.add(currentField.toString().trim());
+
+        return result;
     }
 }
--- src/main/java/org/example/pharmaticb/utilities/DateUtil.java
+++ src/main/java/org/example/pharmaticb/utilities/DateUtil.java
@@ -1,21 +1,21 @@
 package org.example.pharmaticb.utilities;
 
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 
 public class DateUtil {
     public static final ZoneId ZONE_ID_DHAKA = ZoneId.of("Asia/Dhaka");
     public static final String MW_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss:SSS Z";
     public static final String DB_TIME_STAMP_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSX";
-    public static final String TransactionIdPattern = "yyyy-MM-dd";
+    public static final String TRANSACTION_ID_PATTERN = "yyyy-MM-dd";
+    public static final String BULK_PRODUCT_INPUT_FORMAT = "M/d/yyyy";
 
     public static final DateTimeFormatter MW_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(MW_DATE_TIME_FORMAT);
     private static final String OTP_SMS_HOUR_MINUTE_FORMAT = "HHmm";
     private static final DateTimeFormatter OTP_SMS_HOUR_MINUTE_FORMATTER = DateTimeFormatter.ofPattern(OTP_SMS_HOUR_MINUTE_FORMAT);
     private static final DateTimeFormatter DB_TIME_STAMP_FORMATTER = DateTimeFormatter.ofPattern(DB_TIME_STAMP_FORMAT);
-    private static final DateTimeFormatter TRANSACTION_ID_FORMATTER = DateTimeFormatter.ofPattern(TransactionIdPattern);
+    private static final DateTimeFormatter TRANSACTION_ID_FORMATTER = DateTimeFormatter.ofPattern(TRANSACTION_ID_PATTERN);
+    public static final DateTimeFormatter BULK_PRODUCT_INPUT_FORMATTER = DateTimeFormatter.ofPattern(BULK_PRODUCT_INPUT_FORMAT);
 
     public static String formattedDateTime() {
         return MW_DATE_TIME_FORMATTER.format(getZonedDateTime());
@@ -35,6 +35,11 @@ public class DateUtil {
 
     public static String currentTimeInDBTimeStamp() {
         return DB_TIME_STAMP_FORMATTER.format(getZonedDateTime());
+    }
+
+    public static String productExpiresFormat(LocalDate inputDate) {
+        ZonedDateTime zonedDateTime = inputDate.atStartOfDay(ZONE_ID_DHAKA);
+        return DB_TIME_STAMP_FORMATTER.format(zonedDateTime);
     }
 
     public static String getTransactionIdDate() {
